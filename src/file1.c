53e51527e3bb4e7754112219ea9df405c15f24b3
yakovypg <yasha-pmp@mail.ru>
2023-03-19 11:47:22 +0300

    Add new func for printing messages

---
+++ src/file1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void func1() {
+	printf("Hello from file1.c!\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include "file1.h"
 
 int main() {
 	printf("Hello from main!\n");
+	func1();
+
+	return 0;
 }
